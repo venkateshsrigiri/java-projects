a8738370f8b3f98c64d0669ba1472ace962d5411
venkateshsrigiri <104309202+venkateshsrigiri@users.noreply.github.com>
2025-10-10 13:46:54 +0530

    Add files via upload

---
+++ ZeroesToEnd.java
@@ -0,0 +1,20 @@
+public class ZeroesToEnd {
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 0, 3, 12};
+        int index = 0;
+
+        for(int i = 0;i<arr.length;i++){
+            if(arr[i] != 0){
+                arr[index++] = arr[i];
+            }
+        }
+        while(index<arr.length){
+            arr[index++] = 0;
+        }
+        for(int num : arr){
+            System.out.println(num);
+        }
+
+    }
+
+}
